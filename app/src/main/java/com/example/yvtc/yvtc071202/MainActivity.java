bf73b099161153b6206b3d99252033dcb1e2e90d
Shaojung <shaojung@gmail.com>
2017-07-12 15:09:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc071202/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.yvtc.yvtc071202;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = new Button(MainActivity.this);
+        btn.setText("Click Me");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click Me!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button btn2 = new Button(MainActivity.this);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "2222", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn2.setText("Click Me 222");
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
+        layout.addView(btn);
+        layout.addView(btn2);
+    }
+}
